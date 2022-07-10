97582cf03b0523ca6ab6778816b3175465c921d0
alisyabob <sjs939883@gmail.com>
2022-07-10 22:15:39 +0900

    [FIX] repository명 수정

--- src/main/java/com/example/springelasticsearch/config/ElasticsearchApplication.java
+++ src/main/java/com/example/springelasticsearch/config/ElasticsearchApplication.java
@@ -1,6 +1,6 @@
 package com.example.springelasticsearch.config;
 
-import com.example.springelasticsearch.repository.UserSearchRepository;
+import com.example.springelasticsearch.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories(excludeFilters = @ComponentScan.Filter(
         type = FilterType.ASSIGNABLE_TYPE,
-        classes = UserSearchRepository.class))
+        classes = UserRepository.class))
 @SpringBootApplication
 public class ElasticsearchApplication {
 
