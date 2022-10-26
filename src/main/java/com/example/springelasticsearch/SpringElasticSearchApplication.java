207231fcd6d4929173e39e7a0ce1c9b32d5ab6bf
DESKTOP-B76L9MB\User <sjs939883@gmail.com>
2022-10-26 18:21:18 +0900

    [FEAT] JPA Repository - UserRepository Bean 활성화

--- src/main/java/com/example/springelasticsearch/SpringElasticSearchApplication.java
+++ src/main/java/com/example/springelasticsearch/SpringElasticSearchApplication.java
@@ -1,8 +1,15 @@
 package com.example.springelasticsearch;
 
+import com.example.springelasticsearch.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories(excludeFilters = @ComponentScan.Filter(
+        type = FilterType.ASSIGNABLE_TYPE,
+        classes = UserRepository.class))
 @SpringBootApplication
 public class SpringElasticSearchApplication {
 
